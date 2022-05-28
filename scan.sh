46da3c34b80cf057e3de98fb5ddb0e68a64c65f4
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-28 00:02:48 -0500

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -9,34 +9,13 @@ NC='\033[0m'
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-
-IP=$()
-
-
-echo "${GREEN}${bold} install tools?${NC}${normal} (y/n)"
-read -r reply
-  if [ "$reply" = y ] || [ "$reply" = Y ]
-   then
-      sudo apt-get install nmap
-    else
-       echo "${RED}${bold} nmap, not installed${NC}${normal}"
-    fi
         
 echo "${GREEN}${bold} scan domain or IP${NC}${normal} (y/n)"
 read -r reply
   if [ "$reply" = y ] || [ "$reply" = Y ]
    then
-      read -p "Enter Domain or IP: " fullname
+      read -p "Enter Domain or IP: " IP
       nmap -F ${IP}
-    else
-       echo "${RED}${bold} nmap, not installed${NC}${normal}"
-    fi
-
-echo "${GREEN}${bold} scan for a file?${NC}${normal} (y/n)"
-read -r reply
-  if [ "$reply" = y ] || [ "$reply" = Y ]
-   then
-      sudo apt-get install nmap
     else
        echo "${RED}${bold} nmap, not installed${NC}${normal}"
     fi
