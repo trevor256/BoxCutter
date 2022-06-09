459797c59d10c92bd56001d4ce5f7e79433203f5
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-09 13:47:54 -0500

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -9,13 +9,33 @@ NC='\033[0m'
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-        
+echo "${GREEN}${bold} Install nmap Recon-Ng TheHarvester Google Dorks?${NC}${normal} (y/n)"
+read -r reply
+  if [ "$reply" = y ] || [ "$reply" = Y ]
+   then
+     sudo apt install nmap recon-ng TheHarvester Google Dorks
+    else
+       echo "${RED}${bold} nmap, not installed${NC}${normal}"
+    fi
+
+
 echo "${GREEN}${bold} Scan Domain or IP?${NC}${normal} (y/n)"
 read -r reply
   if [ "$reply" = y ] || [ "$reply" = Y ]
    then
       read -p "Enter Domain or IP: " IP
-      nmap -Pn ${IP}
+      nmap -sS -Pn ${IP}
+    else
+       echo "${RED}${bold} nmap, not installed${NC}${normal}"
+    fi
+
+
+echo "${GREEN}${bold} Scan for OSI?${NC}${normal} (y/n)"
+read -r reply
+  if [ "$reply" = y ] || [ "$reply" = Y ]
+   then
+      read -p "Enter name, email, or other PII: " PII
+      recon-cli --stealth ${PII}
     else
        echo "${RED}${bold} nmap, not installed${NC}${normal}"
     fi
