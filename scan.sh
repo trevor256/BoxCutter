924046c009f58c7f10522d6fedef320210c5cf7d
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-07-07 11:30:18 -0500

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -9,22 +9,15 @@ NC='\033[0m'
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo "${GREEN}${bold} Install nmap Recon-Ng TheHarvester Google Dorks?${NC}${normal} (y/n)"
-read -r reply
-  if [ "$reply" = y ] || [ "$reply" = Y ]
-   then
-     sudo apt install nmap recon-ng TheHarvester Google Dorks
-    else
-       echo "${RED}${bold} nmap, not installed${NC}${normal}"
-    fi
-
-
 echo "${GREEN}${bold} Scan Domain or IP?${NC}${normal} (y/n)"
 read -r reply
   if [ "$reply" = y ] || [ "$reply" = Y ]
    then
       read -p "Enter Domain or IP: " IP
-      nmap -sS -Pn ${IP}
+      nslookup
+      dig
+      host
+      
     else
        echo "${RED}${bold} nmap, not installed${NC}${normal}"
     fi
